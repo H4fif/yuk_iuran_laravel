46914779cda06cb02ddfe2b4606325d9a0fad33b
Hafif I <hafifimammuddyn@gmail.com>
2021-03-08 08:22:43 +0700

    feat: add new model for villages table with functions to get all data and get data by id

---
+++ app/Village.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Village extends Model
+{
+    protected $table = 'villages';
+    protected $fillable = [];
+
+    /**
+     * Define relationship with district table
+     */
+    public function district() {
+        return $this->hasMany('App\District');
+    }
+
+    /**
+     * Get all records from villages table
+     * Default limit 100
+     */
+    public function getAll($limit = 100) {
+        return Village::select(
+                      'villages.id',
+                      'provinces.name as province',
+                      'regencies.name as regency',
+                      'districts.name as district',
+                      'villages.name as village'
+                  )
+                    ->join('districts', 'villages.district_id', '=', 'districts.id')
+                    ->join('regencies', 'regencies.id', '=', 'districts.regency_id')
+                    ->join('provinces', 'provinces.id', '=', 'regencies.province_id')
+                    ->limit($limit)
+                    ->get();
+    }
+
+    /**
+     * Get a record from villages table by specified id
+     */
+    public function getById($id) {
+        return Village::select(
+                      'villages.id',
+                      'provinces.name as province',
+                      'regencies.name as regency',
+                      'districts.name as district',
+                      'villages.name as village'
+                  )
+                    ->join('districts', 'villages.district_id', '=', 'districts.id')
+                    ->join('regencies', 'regencies.id', '=', 'districts.regency_id')
+                    ->join('provinces', 'provinces.id', '=', 'regencies.province_id')
+                    ->where('villages.id', '=', $id)
+                    ->get();
+    }
+}
