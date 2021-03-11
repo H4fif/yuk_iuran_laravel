6bf24d5cef4a47d1cebce7115fda3189161ce625
Hafif I <hafifimammuddyn@gmail.com>
2021-03-11 18:23:40 +0700

    changes: update routes for api and regular routes

--- routes/web.php
+++ routes/web.php
@@ -12,10 +12,23 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard.index');
+});
+
+Route::get('/index', function () {
+  return view('templates.index');
 });
 
 Route::prefix('user')->group(function () {
     Route::get('/', ['as' => 'user.index', 'uses' => 'UserController@index']);
-    Route::post('/create', ['as' => 'user.store', 'uses' => 'UserController@store']);
+    Route::get('/sign-up', ['as' => 'user.signup', 'uses' => 'UserController@signUpPage']);
+});
+
+
+// API
+Route::prefix('api')->group(function () {
+    Route::prefix('user')->group(function () {
+        Route::get('/get', ['as' => 'api.user.get', 'uses' => 'UserController@getAll']);
+        Route::post('/create', ['as' => 'api.user.store', 'uses' => 'UserController@store']);
+    });
 });
